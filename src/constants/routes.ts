0571dc8ef242d8ff251ea386a803b179535df22c
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-04-08 00:18:15 +0300

    feat: create routes constant

---
+++ src/constants/routes.ts
@@ -0,0 +1,10 @@
+export const ROUTES = {
+  HOME: "/",
+  ABOUT: "/about",
+  SERVICES: "/services",
+  EQUIPMENT: "/equipment",
+  CONTACT: "/contact",
+  PROFILE: "/profile",
+  LOGIN: "/login",
+  NOTFOUND: "*",
+};
