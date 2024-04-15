53bdae070771ae688e38fcac4a9b97f2c0932b57
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-04-15 14:08:20 +0300

    feat: create global styles and use theme provider in app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { NAV } from "../constants/nav";
 import { Navbar } from "./Navbar";
+import { ThemeProvider } from "styled-components";
+import { THEME } from "../constants/theme";
+import { Global } from "../styled";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        {NAV.map((nav) => (
-          <Route key={nav.name} path={nav.path} element={<nav.component />} />
-        ))}
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={THEME}>
+      <BrowserRouter>
+        <Global />
+        <Navbar />
+        <Routes>
+          {NAV.map((nav) => (
+            <Route key={nav.name} path={nav.path} element={<nav.component />} />
+          ))}
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
