ec1e973d38900f407554efd5da44f80f5a95072c
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-04-16 01:22:03 +0300

    feat: create map constant

---
+++ src/constants/map.ts
@@ -0,0 +1,13 @@
+export const INITIAL_MAP: IMapboxMap = {
+  center: [23.755149424805474, 53.63433166327404],
+  style: "mapbox://styles/mapbox/streets-v11",
+  zoom: 11,
+  attributionControl: false,
+};
+
+export const LOCATION_DATA: ILocation = {
+  title: "ИП Колонтай",
+  address: "д. Добровольщина, ул. Новая, д. 4",
+  latitude: "53.625015",
+  longitude: "23.708364",
+};
