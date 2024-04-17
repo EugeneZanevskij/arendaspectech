943311fb61483e1dbc52423d45da2402433fd6b0
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-04-18 02:24:03 +0300

    feat: create ADVANTAGES constant

---
+++ src/constants/advantages.ts
@@ -0,0 +1,45 @@
+import { FaScrewdriverWrench } from "react-icons/fa6";
+import { GiReceiveMoney } from "react-icons/gi";
+import { AiOutlineSafety } from "react-icons/ai";
+import { IoDocumentsSharp } from "react-icons/io5";
+import { IoIosPeople } from "react-icons/io";
+import { FaShippingFast } from "react-icons/fa";
+
+export const ADVANTAGES = [
+  {
+    id: 1,
+    title: "Квалифицированные водители-операторы",
+    text: "Опытные машинисты и менеджеры, регулярно повышающие квалификацию",
+    icon: FaScrewdriverWrench,
+  },
+  {
+    id: 2,
+    title: "Гибкая форма оплаты",
+    text: "Наличный и безналичный расчет",
+    icon: GiReceiveMoney,
+  },
+  {
+    id: 3,
+    title: "Надежность",
+    text: "Наша техника всегда обслужена, заправлена и готова к работе 24/7",
+    icon: AiOutlineSafety,
+  },
+  {
+    id: 4,
+    title: "Полноценный документооборот",
+    text: "Полный перечень бухгалтерских документов и отчетности в срок",
+    icon: IoDocumentsSharp,
+  },
+  {
+    id: 5,
+    title: "Сотрудничаем с компаниями и частными лицами",
+    text: "Сотрудничаем с компаниями и частными лицами в городе Гродно и Гродненском районе",
+    icon: IoIosPeople,
+  },
+  {
+    id: 6,
+    title: "Быстрая подача",
+    text: "Мы доставляем технику на объект уже в день обращения",
+    icon: FaShippingFast,
+  },
+];
