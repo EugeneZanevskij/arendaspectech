96ba57296a07e1b9d7ce4816d3a0203b33a954b0
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-04-21 01:44:17 +0300

    feat: add ServicesSection component on home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,19 @@
 import { AboutSection } from "../../components/AboutSection";
 import { Advantages } from "../../components/Advantages";
 import { HomeHero } from "../../components/HomeHero";
+import { ServicesItems } from "../../components/ServicesItems";
+import { ServicesSection } from "../../components/ServicesSection";
 import { WorkStages } from "../../components/WorkStages";
+import { SERVICES } from "../../constants/services";
 
 export const Home = () => {
   return (
     <>
       <HomeHero />
       <Advantages />
+      <ServicesSection>
+        <ServicesItems services={SERVICES} />
+      </ServicesSection>
       <WorkStages />
       <AboutSection />
     </>
