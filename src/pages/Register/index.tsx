781d6e2d2fa1a173f7dac31186aa6e1582861ee1
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-04-28 00:12:47 +0300

    feat: create register page and import axios, react-input-mask dependencies

---
+++ src/pages/Register/index.tsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { getEnvVars } from "../../constants/env";
+import {
+  Input,
+  Label,
+  LoginButton,
+  RegisterContainer,
+  RegisterForm,
+  SubmitButton,
+  TelInput,
+  Title,
+} from "./styled";
+
+const backendRoute = getEnvVars("backendRoute");
+
+export const Register = () => {
+  const navigate = useNavigate();
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    phone: "",
+    password: "",
+  });
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    axios
+      .post(`${backendRoute}/api/register`, values)
+      .then((response) => {
+        if (response.status === 201) {
+          navigate("/login");
+        } else {
+          alert(response.data.error);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <RegisterContainer>
+      <RegisterForm onSubmit={handleSubmit}>
+        <Title>Регистрация</Title>
+        <Label htmlFor="username">Имя и Фамилия</Label>
+        <Input
+          id="username"
+          type="text"
+          placeholder="Введите ваше имя и фамилию"
+          value={values.username}
+          onChange={(e) => setValues({ ...values, username: e.target.value })}
+        />
+        <Label htmlFor="email">Email</Label>
+        <Input
+          id="email"
+          type="email"
+          placeholder="Введите ваш email"
+          value={values.email}
+          onChange={(e) => setValues({ ...values, email: e.target.value })}
+        />
+        <Label htmlFor="tel">Номер телефона</Label>
+        <TelInput
+          id="tel"
+          mask="+375 (99) 999-99-99"
+          placeholder="Введите ваш номер"
+          value={values.phone}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setValues({ ...values, phone: e.target.value })
+          }
+        />
+        <Label htmlFor="password">Пароль</Label>
+        <Input
+          id="password"
+          type="password"
+          placeholder="Введите ваш пароль"
+          value={values.password}
+          onChange={(e) => setValues({ ...values, password: e.target.value })}
+        />
+        <SubmitButton type="submit">Регистрация</SubmitButton>
+        <LoginButton to="/login">Login</LoginButton>
+      </RegisterForm>
+    </RegisterContainer>
+  );
+};
---
+++ src/pages/Register/styled.tsx
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+import InputMask from "react-input-mask";
+import { Link } from "react-router-dom";
+
+export const RegisterContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+export const RegisterForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  padding: 1.5rem;
+  border: 1px solid #ccc;
+  border-radius: 1rem;
+`;
+
+export const Title = styled.h2`
+  margin-bottom: 1.25rem;
+`;
+
+export const Label = styled.label`
+  font-size: 0.875rem;
+  margin-bottom: 0.25rem;
+`;
+
+export const Input = styled.input`
+  margin-bottom: 1rem;
+  padding: 0.75rem;
+  border: 1px solid #ccc;
+  border-radius: 0.5rem;
+  width: 15rem;
+`;
+
+export const TelInput = styled(InputMask)`
+  margin-bottom: 1rem;
+  padding: 0.75rem;
+  border: 1px solid #ccc;
+  border-radius: 0.5rem;
+  width: 15rem;
+`;
+
+export const SubmitButton = styled.button`
+  padding: 0.75rem;
+  background-color: ${({ theme }) => theme.orange};
+  font-size: 1rem;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+export const LoginButton = styled(Link)`
+  margin-top: 1rem;
+  padding: 0.75rem;
+  background-color: ${({ theme }) => theme.black};
+  font-size: 1rem;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  text-decoration: none;
+  text-align: center;
+  cursor: pointer;
+`;
