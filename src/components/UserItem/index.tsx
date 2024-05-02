a69aed83d4042541a533bac366fee4247e42423e
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-05-02 22:57:50 +0300

    feat: update UserItem so that it will show data based on props

--- src/components/UserItem/index.tsx
+++ src/components/UserItem/index.tsx
@@ -1,4 +1,3 @@
-import { useParams } from "react-router-dom";
 import {
   BackButton,
   BoldText,
@@ -7,33 +6,25 @@ import {
   Text,
   UserDataWrapper,
 } from "./styled";
-import { useEffect, useState } from "react";
-import axiosInstance from "../../api/axiosInstance";
 import { IUserData } from "../../types";
+import { MouseEventHandler } from "react";
 
-export const UserItem = () => {
-  const [user, setUser] = useState<IUserData>({} as IUserData);
-  const { userId } = useParams();
+interface UserItemProps {
+  user: IUserData;
+  handleEdit: MouseEventHandler<HTMLButtonElement>;
+  handleDelete: MouseEventHandler<HTMLButtonElement>;
+  isAdmin: boolean;
+}
 
-  useEffect(() => {
-    axiosInstance.get(`/users/${userId}`).then((res) => {
-      setUser(res.data);
-    });
-  }, [userId]);
-
-  const handleEdit = () => {
-    // Implement your logic to edit the user
-    console.log("Editing user:", user.id);
-  };
-
-  const handleDelete = () => {
-    // Implement your logic to delete the user
-    console.log("Deleting user:", user.id);
-  };
-
+export const UserItem = ({
+  user,
+  handleEdit,
+  handleDelete,
+  isAdmin,
+}: UserItemProps) => {
   return (
     <UserDataWrapper>
-      <BackButton to="/users">Назад</BackButton>
+      {isAdmin && <BackButton to="/users">Назад</BackButton>}
       <Text fontsize="1.5rem">
         <BoldText>Имя: {user.username}</BoldText>
       </Text>
