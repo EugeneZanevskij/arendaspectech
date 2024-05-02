b116b4bd8779f44a1304943f210e38d3aa0c8bd2
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-05-02 22:59:05 +0300

    feat: update types in UserSlices

--- src/slices/userSlice.ts
+++ src/slices/userSlice.ts
@@ -1,17 +1,9 @@
 import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axiosInstance from "../api/axiosInstance";
-
-type UserProfileData = {
-  id: string;
-  username: string;
-  email: string;
-  phone: string;
-  password: string;
-  isAdmin: boolean;
-};
+import { IUserData } from "../types";
 
 type UserApiState = {
-  userProfileData?: UserProfileData | null;
+  userProfileData?: IUserData | null;
   status: "idle" | "loading" | "failed";
   error: string | null;
 };
@@ -40,13 +32,10 @@ const userSlice = createSlice({
         state.status = "loading";
         state.error = null;
       })
-      .addCase(
-        getUser.fulfilled,
-        (state, action: PayloadAction<UserProfileData>) => {
-          state.status = "idle";
-          state.userProfileData = action.payload;
-        },
-      )
+      .addCase(getUser.fulfilled, (state, action: PayloadAction<IUserData>) => {
+        state.status = "idle";
+        state.userProfileData = action.payload;
+      })
       .addCase(getUser.rejected, (state, action) => {
         state.status = "failed";
         state.error = action.error.message || "Get user profile data failed";
