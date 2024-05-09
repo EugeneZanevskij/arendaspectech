1615b5cb0ebd45fea2930a0923624c2bfbfa4188
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-05-09 23:32:45 +0300

    feat: create EquipmentToServicesForm component

---
+++ src/components/EquipmentToServicesForm/index.tsx
@@ -0,0 +1,129 @@
+import React, { useEffect, useState } from "react";
+import {
+  ICategoryFull,
+  IEquipmentFullExtended,
+  IEquipmentToServices,
+} from "../../types";
+import axiosInstance from "../../api/axiosInstance";
+import styled from "styled-components";
+import CategorySelectComponent from "../CategorySelectComponent";
+
+interface EquipmentToServicesFormProps {
+  equipmentToServicesId: number;
+  onEquipmentToServicesIdChange: (newId: number) => void;
+}
+
+const Form = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  color: ${({ theme }) => theme.white};
+`;
+
+const Label = styled.label`
+  font-weight: bold;
+`;
+
+const Button = styled.button`
+  padding: 5px 10px;
+  background-color: ${({ theme }) => theme.orange};
+  color: white;
+  border: none;
+  cursor: pointer;
+`;
+
+export const EquipmentToServicesForm = ({
+  equipmentToServicesId,
+  onEquipmentToServicesIdChange,
+}: EquipmentToServicesFormProps) => {
+  const [values, setValues] = useState<IEquipmentToServices>(
+    {} as IEquipmentToServices,
+  );
+  const [allEquipment, setAllEquipment] = useState<IEquipmentFullExtended[]>(
+    [],
+  );
+  const [allServices, setAllServices] = useState<ICategoryFull[]>([]);
+
+  const fetchAllEquipment = async () => {
+    axiosInstance
+      .get(`/admin/equipment`)
+      .then((res) => {
+        setAllEquipment(res.data);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const fetchAllServices = async () => {
+    values.equipmentId &&
+      axiosInstance
+        .get(`/admin/equipment/${values.equipmentId}`)
+        .then((res) => {
+          setAllServices(res.data.services);
+        })
+        .catch((error) => console.log(error));
+  };
+
+  const fetchEquipmentToService = async (id: number) => {
+    axiosInstance
+      .get(`/admin/equipment-to-services/${id}`)
+      .then((res) => {
+        setValues(res.data);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    fetchAllEquipment();
+    fetchEquipmentToService(equipmentToServicesId);
+  }, []);
+
+  useEffect(() => {
+    fetchAllServices();
+    onEquipmentToServicesIdChange(values.id);
+  }, [values.equipmentId]);
+
+  const handleSubmit = async () => {
+    console.log(values);
+    try {
+      const data = await axiosInstance.get(
+        `/admin/equipment-to-services/equipment`,
+        {
+          params: {
+            equipmentId: values.equipmentId,
+            servicesId: values.servicesId,
+          },
+        },
+      );
+      onEquipmentToServicesIdChange(data.data.id);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <Form>
+      <h2>Edit Equipment To Services</h2>
+      <Label>
+        Equipment:
+        <CategorySelectComponent
+          categories={allEquipment}
+          value={values.equipmentId}
+          onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
+            setValues({ ...values, equipmentId: +e.target.value });
+          }}
+        />
+      </Label>
+      <Label>
+        Services:
+        <CategorySelectComponent
+          categories={allServices}
+          value={values.servicesId}
+          onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+            setValues({ ...values, servicesId: +e.target.value })
+          }
+        />
+      </Label>
+      <Button onClick={handleSubmit}>Update EquipmentToServicesId</Button>
+    </Form>
+  );
+};
