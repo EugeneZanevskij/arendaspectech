ffbfe0a46a55041bceb847273ad8e5521d4c5fa9
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-05-11 03:58:25 +0300

    feat: update home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,12 @@
 import { AboutSection } from "../../components/AboutSection";
 import { Advantages } from "../../components/Advantages";
-import { EquipmentsItems } from "../../components/EquipmentItems";
-import { EquipmentSection } from "../../components/EquipmentSection/styled";
+import { EquipmentsTypeItems } from "../../components/EquipmentItems";
+import { EquipmentTypeSection } from "../../components/EquipmentTypeSection";
 import { HomeHero } from "../../components/HomeHero";
 import { ServicesItems } from "../../components/ServicesItems";
 import { ServicesSection } from "../../components/ServicesSection";
 import { WorkStages } from "../../components/WorkStages";
-import { EQUIPMENT } from "../../constants/equipment";
+import { EQUIPMENTTYPES } from "../../constants/equipmentTypes";
 import { SERVICES } from "../../constants/services";
 
 export const Home = () => {
@@ -17,9 +17,9 @@ export const Home = () => {
       <ServicesSection>
         <ServicesItems services={SERVICES} />
       </ServicesSection>
-      <EquipmentSection>
-        <EquipmentsItems equipments={EQUIPMENT} />
-      </EquipmentSection>
+      <EquipmentTypeSection>
+        <EquipmentsTypeItems equipmentTypes={EQUIPMENTTYPES} />
+      </EquipmentTypeSection>
       <WorkStages />
       <AboutSection />
     </>
