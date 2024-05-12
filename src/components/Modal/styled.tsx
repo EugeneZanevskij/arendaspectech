69aaf7a5bad47202d9e838791da706aee8f66544
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-05-12 21:01:53 +0300

    feat: create new styled-components for Modal

--- src/components/Modal/styled.tsx
+++ src/components/Modal/styled.tsx
@@ -12,12 +12,12 @@ export const ModalContainer = styled.div`
   align-items: center;
 `;
 
-export const ModalForm = styled.div`
+export const ModalWrapper = styled.div`
   display: flex;
   flex-direction: column;
   width: fit-content;
   height: fit-content;
-  background-color: ${({ theme }) => theme.darkGray};
+  background-color: ${({ theme }) => theme.suvaGray};
   border: 1px solid ${({ theme }) => theme.black};
   border-radius: 1rem;
   padding: 1.5rem 3rem;
@@ -33,4 +33,37 @@ export const CloseButton = styled.button`
   border: none;
   width: 1rem;
   height: 1rem;
+`;
+
+export const ModalForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  color: ${({ theme }) => theme.white};
+`;
+
+export const ModalHeading = styled.h2`
+  text-align: center;
+  margin-bottom: 0.5rem;
+`;
+
+export const ModalLabel = styled.label`
+  display: flex;
+  flex-direction: column;
+  gap: 0.25rem;
+  font-weight: bold;
+`;
+
+export const ModalInput = styled.input`
+  padding: 0.3125rem;
+  border-radius: 0.5rem;
+`;
+
+export const ModalButton = styled.button`
+  padding: 0.5rem 1rem;
+  background-color: ${({ theme }) => theme.orange};
+  color: ${({ theme }) => theme.white};
+  border-radius: 0.5rem;
+  border: none;
+  cursor: pointer;
 `;
