94bed1b1dad067c3f4c1e4e3b6cb0bec2979090c
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-05-12 03:21:02 +0300

    feat: create ADMIN_LINKS constant

---
+++ src/constants/adminLinks.ts
@@ -0,0 +1,37 @@
+export const ADMIN_LINKS = [
+  {
+    id: 1,
+    title: "Главная",
+    link: "/admin",
+  },
+  // {
+  //   id: 2,
+  //   title: "Пользователи",
+  //   link: "/admin/users",
+  // },
+  {
+    id: 3,
+    title: "Услуги",
+    link: "/admin/services",
+  },
+  {
+    id: 4,
+    title: "Типы оборудования",
+    link: "/admin/equipment-type",
+  },
+  {
+    id: 5,
+    title: "Техника",
+    link: "/admin/equipment",
+  },
+  {
+    id: 6,
+    title: "Статусы",
+    link: "/admin/status",
+  },
+  {
+    id: 7,
+    title: "Бронирования",
+    link: "/admin/bookings",
+  },
+];
