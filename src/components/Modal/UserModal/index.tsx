7a9d2ac0dbdb12c619b33231c6c205c08fe7c34c
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-05-12 21:53:09 +0300

    refactor: move UserModal to modal folder and use styled for modal

---
+++ src/components/Modal/UserModal/index.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+// import { useDispatch } from "react-redux";
+// import { useNavigate } from "react-router-dom";
+// import { updateUser } from "../../slices/userSlice";
+import { Modal } from "../index";
+import { IUserData } from "../../../types";
+import axiosInstance from "../../../api/axiosInstance";
+import {
+  ModalButton,
+  ModalForm,
+  ModalHeading,
+  ModalInput,
+  ModalLabel,
+} from "../styled";
+
+interface UserModalProps {
+  user: IUserData;
+  onClose: () => void;
+}
+
+export const UserModal = ({ user, onClose }: UserModalProps) => {
+  const [values, setValues] = useState<IUserData>({ ...user });
+  // const dispatch = useDispatch();
+  // const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      await axiosInstance.put(`/users/${user.id}`, values);
+      // navigate("/users");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <Modal id="user-modal" onClose={onClose}>
+      <ModalForm onSubmit={handleSubmit}>
+        <ModalHeading>Редактировать пользователя</ModalHeading>
+        <ModalLabel htmlFor="username">
+          Username:
+          <ModalInput
+            type="text"
+            id="username"
+            name="username"
+            value={values.username}
+            onChange={handleChange}
+          />
+        </ModalLabel>
+        <ModalLabel htmlFor="email">
+          E-mail:
+          <ModalInput
+            type="email"
+            id="email"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+          />
+        </ModalLabel>
+        <ModalLabel htmlFor="phone">
+          Номер телефона:
+          <ModalInput
+            type="text"
+            id="phone"
+            name="phone"
+            value={values.phone}
+            onChange={handleChange}
+          />
+        </ModalLabel>
+        <ModalButton type="submit">Обновить</ModalButton>
+      </ModalForm>
+    </Modal>
+  );
+};
