aa8a47c1ef8a8e8cb3540fe60b87368444e53513
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-05-16 23:03:34 +0300

    feat: add link to /admin for admin

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,8 +1,7 @@
 import { useEffect } from "react";
 import { SectionContainer } from "../../components/SectionContainer";
 import { SectionHeading } from "../../components/SectionHeading";
-import { Link } from "react-router-dom";
-import { LogoutButton } from "./styled";
+import { OrangeLink, LogoutButton } from "./styled";
 import { useAppDispatch, useAppSelector } from "../../hooks/redux-hooks";
 import { logout } from "../../slices/authSlice";
 import { getUser } from "../../slices/userSlice";
@@ -38,13 +37,16 @@ export const Profile = () => {
         <SectionHeading {...sectionHeading} color={true} />
         {userProfileInfo ? (
           <>
+            {userProfileInfo.isAdmin && (
+              <OrangeLink to="/admin">Панель администратора</OrangeLink>
+            )}
             <UserItem user={userProfileInfo} isAdmin={false} />
             <LogoutButton onClick={handleLogout}>Выйти</LogoutButton>
           </>
         ) : (
           <>
             <h1>Авторизация</h1>
-            <Link to="/login">Авторизация</Link>
+            <OrangeLink to="/login">Авторизация</OrangeLink>
           </>
         )}
       </>
