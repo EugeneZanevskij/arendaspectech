8d2ee1e73a041f2b7ba8f7a2a7a0e2525f94ea2f
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-06-03 11:12:19 +0300

    fix: fix default layout and index.html

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -3,7 +3,7 @@ import { useAppSelector } from "../hooks/redux-hooks";
 import { Navigate } from "react-router-dom";
 
 const DefaultLayout = () => {
-  const basicUserInfo = useAppSelector((state) => state.auth.basicUserInfo);
+  const basicUserInfo = useAppSelector((state) => state.user.userProfileData);
 
   if (basicUserInfo) {
     return <Navigate replace to={"/profile"} />;
